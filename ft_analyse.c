b3ea4db58216f1bb803443b817ec47ecfeff6e69
Paul TRUFFAULT <ptruffau@e2r7p5.42.fr>
2017-08-22 21:59:15 +0200

    fix bug

--- ft_analyse.c
+++ ft_analyse.c
@@ -6,90 +6,82 @@
 /*   By: ptruffau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/21 17:35:47 by ptruffau          #+#    #+#             */
-/*   Updated: 2017/08/22 06:58:17 by ptruffau         ###   ########.fr       */
+/*   Updated: 2017/08/22 21:58:43 by ptruffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "pointh.h"
-
+void	ft_init_struct(t_save *save)
+{
+	int i;
+	int j;
+	save->x_max = 6;
+	save->y_max = 6;
+	save->dim = 0;
+	save->map = (int**)malloc(sizeof(int*) * (save->y_max + 18));
+	i = 0;
+	while (i < save->y_max)
+	{
+		save->map[i] = (int*)malloc(sizeof(int) * (save->x_max + 18));
+		j = 0;
+		while (j < save->x_max)
+		{
+			save->map[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	save->map[5][0] = 1;
+}
 void teststr(t_save save)
 {
 	// test de toutes les valeurs de la structure 
 	printf("x = %d \ny = %d \ndim = %d \nxmax = %d\nymax = %d \n\n",save.x,save.y,save.dim,save.x_max,save.y_max);
 }
 
-int		ft_analyse(t_save save)
+void	ft_analyse(t_save *save)
 {
 	int i;
 	int j;
-	int test;
 	int x;
 	int y;
-	int map[9][9]={{0, 0, 0, 0, 0, 1, 0, 0, 0},
-	               {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	               {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	               {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	               {0, 0, 1, 0, 0, 0, 0, 0, 0},
-	               {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	               {1, 0, 0, 0, 0, 0, 0, 0, 0},
-	               {0, 0, 0, 0, 0, 0, 0, 1, 0},
-	               {0, 0, 0, 0, 0, 0, 0, 0, 1}};
 
 	y = 0;
-	while (y < save.y_max - save.dim)
+	while (y < save->y_max - save->dim)
 	{
 		x = 0;
-		while (x < save.x_max - save.dim)
+		while (x < save->x_max - save->dim)
 		{
-			j = 0;
-			i = 1;
-			test = 0;
-			while (test == 0 && i <= save.x_max && i <= save.y_max)
-			{
-				j = i;
-				while (j >= 0)
-				{
-					if (map[x + i - j][y + i] == 1 || map[x + i][y + i - j] == 1)
-						test = 1;
-					j--;
-				}
-			i++;
-			}
-			if (i - 1 > save.dim)
-			{
-				// maj de la structure (apres avoir trouver un plus gros carre)
-				save.dim = i - 1;
-				save.x = x;
-				save.y = y;
-			}
+			if (save->map[y][x] != 1)
+				ft_jaime_les_carres(save, x, y);		
 			x++;
 		}
 		y++;
 	}
-//	teststr(save);
+	teststr(*save);
 
 	//dessin du carre
-	i = save.y;
-	j = save.x;
-	while (i < save.y + save.dim)
+	i = save->y;
+	j = save->x;
+	while (i < save->y + save->dim)
 	{
-		j = save.x;
-		while (j < save.x + save.dim)
+		j = save->x;
+		while (j < save->x + save->dim)
 		{
-			map[i][j] = 7;
+			save->map[i][j] = 7;
 			j++;
 		}
 		i++;
 	}
 	//affichage de map
 	i = 0;
-	while (i < save.y_max)
+	while (i < save->y_max)
 	{
 		j = 0;
-		while (j <  save.x_max)
+		while (j <  save->x_max)
 		{
-			printf("%i ",map[i][j]);
+			printf("%i ",save->map[i][j]);
 			j++;
 		}
 		printf("\n");
@@ -97,14 +89,39 @@ int		ft_analyse(t_save save)
 	}
 }
 
+void	ft_jaime_les_carres(t_save *save, int x, int y)
+{
+	int j;
+	int i;
+	int test;
+
+	j = 0;
+	i = 1;
+	test = 0;
+	while (test == 0 && i <= save->x_max && i <= save->y_max)
+	{
+		j = i;
+		while (j >= 0)
+		{
+			if (save->map[y + i - j][x + i] == 1 || save->map[y + i][x + i - j] == 1)
+				test = 1;
+			j--;
+		}
+		i++;
+	}
+	if (i - 1 > save->dim)
+	{
+		save->dim = i - 1;
+		save->x = x;
+		save->y = y;
+	}
+}
+
 
 int main()
 {
-	//   initialisation de la structure 
-	t_save save;
-	save.x_max = 9;
-	save.y_max = 9;
-	save.dim = 0;
-	ft_analyse(save);
+	t_save save;	
+	ft_init_struct(&save);
+	ft_analyse(&save);
 	return (0);
 }
--- pointh.h
+++ pointh.h
@@ -6,12 +6,13 @@
 /*   By: ptruffau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 03:16:27 by ptruffau          #+#    #+#             */
-/*   Updated: 2017/08/22 05:56:44 by ptruffau         ###   ########.fr       */
+/*   Updated: 2017/08/22 21:13:44 by ptruffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef POINTH_H
 # define POINTH_H
+#include <stdlib.h>
 
 typedef struct		s_save
 {
@@ -20,5 +21,7 @@ typedef struct		s_save
 	int dim;
 	int x_max;
 	int y_max;
+	int **map;
 }					t_save;
+void	ft_jaime_les_carres(t_save *save, int x, int y);
 #endif
